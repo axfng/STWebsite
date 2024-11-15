cdba4ba78afbfd5b0f50cee7036b81a64130eff6
axfng <alexlfeng22@gmail.com>
2024-11-15 14:12:02 -0500

    initialize angular project

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  public static Route = {
+    path: '/home',
+    component: HomeComponent
+  };
+
+  // constructor(route: ActivatedRoute, router: Router) {
+  //   router.navigateByUrl('/home');
+  // }
+}
