d57e5a371771f98170950d27cf2f7242cede5362
axfng <alexlfeng22@gmail.com>
2024-12-18 14:22:22 -0500

    Update assests folder routing to adhere to standards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,11 +5,11 @@ import { ScienceSparknotesComponent } from './science-sparknotes/science-sparkno
 import { JournalComponent } from './journal/journal.component';
 import { MissionComponent } from './mission/mission.component';
 import { GetInvolvedComponent } from './get-involved/get-involved.component';
-import { HealthAndMedComponent } from './health-and-med/health-and-med.component';
-import { ChemAndBiochemComponent } from './chem-and-biochem/chem-and-biochem.component';
-import { MattAndAstroComponent } from './matt-and-astro/matt-and-astro.component';
-import { SociAndPsychComponent } from './soci-and-psych/soci-and-psych.component';
-import { EnvrAndEcoComponent } from './envr-and-eco/envr-and-eco.component';
+import { HealthAndMedComponent } from './science-news/health-and-med/health-and-med.component';
+import { ChemAndBiochemComponent } from './science-news/chem-and-biochem/chem-and-biochem.component';
+import { MattAndAstroComponent } from './science-news/matt-and-astro/matt-and-astro.component';
+import { SociAndPsychComponent } from './science-news/soci-and-psych/soci-and-psych.component';
+import { EnvrAndEcoComponent } from './science-news/envr-and-eco/envr-and-eco.component';
 
 export const routes: Routes = [
     // HomeComponent.Route,
@@ -32,18 +32,18 @@ export const routes: Routes = [
         path: 'get-involved', component: GetInvolvedComponent,
     },
     {
-        path: 'health-and-med', component: HealthAndMedComponent,
+        path: 'science-news/health-and-med', component: HealthAndMedComponent,
     },
     {
-        path: 'chem-and-biochem', component: ChemAndBiochemComponent,
+        path: 'science-news/chem-and-biochem', component: ChemAndBiochemComponent,
     },
     {
-        path: 'matt-and-astro', component: MattAndAstroComponent,
+        path: 'science-news/matt-and-astro', component: MattAndAstroComponent,
     },
     {
-        path: 'soci-and-psych', component: SociAndPsychComponent,
+        path: 'science-news/soci-and-psych', component: SociAndPsychComponent,
     },
     {
-        path: 'envr-and-eco', component: EnvrAndEcoComponent,
+        path: 'science-news/envr-and-eco', component: EnvrAndEcoComponent,
     },
 ];
